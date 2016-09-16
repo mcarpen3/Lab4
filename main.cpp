8303f7164cb18cc081c3934f4a0685e2e13416db
mcarpen3 <mcarpen3@stumail.jccc.edu>
2016-09-15 19:07:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,33 @@ using namespace std;
 
 int main()
 {
-    return 0;
+	const int foodConsumed_PerPersonPerDay = 2;
+	const int oxygenCreated_PerDay = 5;
+	const int wasteCreated_PerPersonPerDay = 3;
+	const int oxygenUsed_PerPersonPerDay = 2;
+	bool done = false;
+	int foodGrown_PerDay;
+	int cleanup_PerPersonPerDay;
+	int foodCount;
+	int wasteCount;
+	int oxygenCount;
+	int peopleCount;
+	int missionLength;
+	string teamName;
+
+	int choice;
+	cout << "---------------------------\n"
+		<< "MARS COLONY MAIN MENU\n"
+		<< "\n"
+		<< "1. Set Mission Parameters\n"
+		<< "2. Run Simulation\n"
+		<< "3. Quit\n"
+		<< "----------------------------\n"
+		<< "Choice >>";
+
+	cin >> choice;
+	
+	if (choice == 3)
+		done = true;
+	return 0;
 }
